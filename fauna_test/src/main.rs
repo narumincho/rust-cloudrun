f53b50ba400e540fab326cd12951e118b987ddbb
narumincho <16481886+narumincho@users.noreply.github.com>
2023-02-01 20:43:49 +0900

    use serde

--- fauna_test/src/main.rs
+++ fauna_test/src/main.rs
@@ -1,19 +1,41 @@
 use async_std;
+use serde_json::json;
 
 #[async_std::main]
 async fn main() -> Result<(), http_types::Error> {
+    let result = request(
+        &include_str!("../../secret.txt"),
+        &Expr::Get(Box::new(Expr::Collections)),
+    )
+    .await?;
+    println!("{}", result);
+    Ok(())
+}
+
+async fn request(secret_key: &str, query: &Expr) -> Result<String, http_types::Error> {
     let string = surf::post("https://db.us.fauna.com")
         .header(
             http_types::headers::AUTHORIZATION,
             format!(
                 "{} {}",
                 http_types::auth::AuthenticationScheme::Bearer,
-                include_str!("../../secret.txt")
+                secret_key
             ),
         )
-        .body("{\"get\":{\"collections\":null}}")
+        .body(expr_to_json_value(&query))
         .recv_string()
         .await?;
-    println!("{}", string);
-    Ok(())
+    Ok(string)
+}
+
+enum Expr {
+    Get(Box<Expr>),
+    Collections,
+}
+
+fn expr_to_json_value(expr: &Expr) -> serde_json::value::Value {
+    match expr {
+        Expr::Get(ref_expr) => json!({ "get": expr_to_json_value(ref_expr) }),
+        Expr::Collections {} => json!({ "collections": null }),
+    }
 }
