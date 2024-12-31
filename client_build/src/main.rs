dca4473fcda443430b8a3cdc236d2d06baee3a13
narumincho <16481886+narumincho@users.noreply.github.com>
2024-12-31 14:50:11 +0900

    wasm-pack を cargo から使う

--- client_build/src/main.rs
+++ client_build/src/main.rs
@@ -1,23 +1,12 @@
 pub fn main() -> anyhow::Result<()> {
-    let output = if cfg!(target_os = "windows") {
-        std::process::Command::new("powershell")
-            .arg("wasm-pack")
-            .arg("build")
-            .arg("./client")
-            .arg("--target")
-            .arg("web")
-            .output()?
-    } else {
-        std::process::Command::new("wasm-pack")
-            .arg("build")
-            .arg("./client")
-            .arg("--target")
-            .arg("web")
-            .output()?
-    };
-
-    println!("stdout: {}", std::str::from_utf8(&output.stdout)?);
-    println!("stderr: {}", std::str::from_utf8(&output.stderr)?);
+    wasm_pack::command::build::Build::run(&mut wasm_pack::command::build::Build::try_from_opts(
+        wasm_pack::command::build::BuildOptions {
+            target: wasm_pack::command::build::Target::Web,
+            path: Some("./client".into()),
+            out_dir: "./pkg".into(),
+            ..Default::default()
+        },
+    )?)?;
 
     let client_js_binary = std::fs::read("./client/pkg/client.js")?;
     let client_js = std::str::from_utf8(&client_js_binary)?;
